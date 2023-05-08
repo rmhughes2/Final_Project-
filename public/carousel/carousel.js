71dc5f5660d2b579d09fc9188ae656e9a03de203
Rachel Hughes <118775322+rmhughes2@users.noreply.github.com>
2023-05-07 19:01:50 -0700

    updatesssss

---
+++ public/carousel/carousel.js
@@ -0,0 +1,42 @@
+const carouselContainer = document.querySelector('.carousel-container');
+const prevBtn = document.querySelector('.prev-btn');
+const nextBtn = document.querySelector('.next-btn');
+const carouselItems = document.querySelectorAll('.carousel-item');
+
+let currentIndex = 0;
+let intervalId;
+
+function startSlideShow() {
+  intervalId = setInterval(() => {
+    currentIndex++;
+    if (currentIndex >= carouselItems.length) {
+      currentIndex = 0;
+    }
+    carouselContainer.style.transform = `translateX(-${currentIndex * 100}%)`;
+  }, 2000);
+}
+
+function stopSlideShow() {
+  clearInterval(intervalId);
+}
+
+prevBtn.addEventListener('click', () => {
+  currentIndex--;
+  if (currentIndex < 0) {
+    currentIndex = carouselItems.length - 1;
+  }
+  carouselContainer.style.transform = `translateX(-${currentIndex * 100}%)`;
+});
+
+nextBtn.addEventListener('click', () => {
+  currentIndex++;
+  if (currentIndex >= carouselItems.length) {
+    currentIndex = 0;
+  }
+  carouselContainer.style.transform = `translateX(-${currentIndex * 100}%)`;
+});
+
+carouselContainer.addEventListener('mouseover', stopSlideShow);
+carouselContainer.addEventListener('mouseleave', startSlideShow);
+
+startSlideShow();
